0cbe06012c57ed0db7b218302418aaf86736d026
Abhay08082000 <bishtabhay8@gmail.com>
2024-10-24 11:42:50 +0530

    streamlit and preprocessing updates

--- common.py
+++ common.py
@@ -12,6 +12,7 @@ import ast
 from langchain_core.tools import Tool
 from langchain_experimental.utilities import PythonREPL
 import time
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 load_dotenv()
 llm=ChatGoogleGenerativeAI(model='gemini-1.0-pro')
 llm1=ChatGoogleGenerativeAI(model='gemini-1.5-pro')
@@ -30,23 +31,24 @@ def data_insights(path):
     return categorical_columns,numeric_columns,missing_value.to_dict()
 def preprocessing(categorical_feature, numerical_columns, missing_value_columns, user_requirement, llm1):
     prompt = '''
-   Given the user requirements, perform the necessary preprocessing steps based on the csv dataset provided, which includes:
+   Given the {user_requirement}, perform the necessary preprocessing steps based on the csv dataset provided, which includes:
 
 Numeric Columns: {numerical_columns}
 Categorical Columns: {categorical_feature}
 Columns with Missing Values: {missing_value_columns}
-Follow the instructions below for data preprocessing:
+based on the above info about dataset follow the instructions below for data preprocessing:
 
-Handle Missing Values: If the dataset contains missing values, address them according to the context of the problem statement. In real-time data, it is not always ideal to impute missing values, so use an appropriate method or leave them if justified. If no missing values exist, proceed to the next steps.
+- Handle Missing Values: If the dataset contains missing values, address them according to the context of the problem statement. In real-time data, it is not always ideal to impute missing values, so use an appropriate method or leave them if justified. If no missing values exist, proceed to the next steps.
 
-Remove Irrelevant Columns: Identify and drop columns  that do not contribute to model performance or irrelevant columns for  example like customerId,name with respect to customer data.Identify the irrelevant columns from {numerical_columns}
+- Remove Irrelevant Columns: Identify and drop columns  that do not contribute to model performance or irrelevant columns for  example like customerId,name with respect to customer data.Identify the irrelevant columns from {numerical_columns}
 and {categorical_feature}.
 
-Data Cleaning: Perform any additional cleaning steps such as removing duplicates, handling outliers, or correcting data formats as required.
+- Data Cleaning: Perform any additional cleaning steps such as removing duplicates, handling outliers, or correcting data formats as required.
 
-Encode Categorical Columns: Use suitable encoding techniques (e.g., One-Hot Encoding or Label Encoding) to convert categorical variables into numeric representations.
+- Encode Categorical Columns: Use suitable encoding techniques (e.g., One-Hot Encoding or Label Encoding) to convert categorical variables into numeric representations.
 
-Scale Numeric Columns: Apply appropriate scaling methods (e.g., StandardScaler, MinMaxScaler) to normalize the numeric features for optimal model performance.
+- Scale Numeric Columns: Apply appropriate scaling methods (e.g., StandardScaler, MinMaxScaler) to normalize the numeric features for optimal model performance.
+- Train_Test_split: Split the Data into Train Test  and save it into a 'project/artifact' folder
     '''
     
     # Create the template
@@ -72,7 +74,7 @@ class CodeOutput(BaseModel):
     description: str =Field(description='Explanation of Preprocessing Code')
     filename: str=Field(description='Give the respective filename')
     
-def Code_to_py(response):
+def parse_output(response):
     parser=PydanticOutputParser(pydantic_object=CodeOutput)
     print(parser.get_format_instructions())
     code_parser_template= """Parse the response from a previous LLM into a description and a string of valid code, 
@@ -80,8 +82,10 @@ def Code_to_py(response):
                                 Here is the response: {response}. You should parse this in the following JSON Format: """
     json_prompt_tmpl=PromptTemplate(template=code_parser_template,input_variables=['response'],output_parser=parser)
     output=llm1.invoke(json_prompt_tmpl.format(response=response))
-    result=output.content.replace("```json", "").replace("```", "")
-    return eval(result)
+    result=output.content
+    print(result)
+    output=result.replace('```json','').replace('```','')
+    return ast.literal_eval(output)
 def code_to_function(code):
     template='''Convert the given code {code} into  function  with saving preprocessing model in pickle with their respective name so
       that it can be used during model prediction.put the explantion string in #  comments'''
@@ -89,15 +93,58 @@ def code_to_function(code):
     return llm.invoke(prompt.format(code=code)).content
 def code_to_file(code,file_name):
     with open(file_name, "w") as f:
-        f.write(code)
+        f.write(code.replace('```python','').replace('```',''))
         f.close()
-def code_execution(file_path,codefile_path):
-    python_repl = PythonREPL()
-    repl_tool = Tool(
-    name="python_repl",
-    description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
-    func=python_repl.run,
-)
+
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+def preprocessing(categorical_feature, numerical_columns, missing_value_columns, user_requirement, llm1):
+    prompt = '''
+   Given the {user_requirement}, perform the necessary preprocessing steps based on the csv dataset provided, which includes:
+
+Numeric Columns: {numerical_columns}
+Categorical Columns: {categorical_feature}
+Columns with Missing Values: {missing_value_columns}
+based on the above info about dataset follow the instructions below for data preprocessing:
+
+- Handle Missing Values: If the dataset contains missing values, address them according to the context of the problem statement. In real-time data, it is not always ideal to impute missing values, so use an appropriate method or leave them if justified. If no missing values exist, proceed to the next steps.
+
+- Remove Irrelevant Columns: Identify and drop columns  that do not contribute to model performance or irrelevant columns for  example like customerId,name with respect to customer data.Identify the irrelevant columns from {numerical_columns}
+and {categorical_feature}.
+
+- Data Cleaning: Perform any additional cleaning steps such as removing duplicates, handling outliers, or correcting data formats as required.
+
+- Encode Categorical Columns: Use suitable encoding techniques (e.g., One-Hot Encoding or Label Encoding) to convert categorical variables into numeric representations.
+
+- Scale Numeric Columns: Apply appropriate scaling methods (e.g., StandardScaler, MinMaxScaler) to normalize the numeric features for optimal model performance.
+- Train_Test_split: Split the Data into Train Test  and save it into a folder
+    '''
+    
+    # Create the template
+    template = PromptTemplate(
+        template=prompt, 
+        input_variables=['categorical_feature', 'numerical_columns', 'missing_value_columns', 'user_requirement']
+    )
+    
+    # Format the prompt
+    formatted = template.format(
+        categorical_feature=categorical_feature, 
+        numerical_columns=numerical_columns, 
+        missing_value_columns=missing_value_columns, 
+        user_requirement=user_requirement
+    )
+    
+    # Call the LLM to get the code based on the formatted prompt
+    output=llm1.invoke(formatted).content
+    return output
+
 
 
 
--- streamlit_page.py
+++ streamlit_page.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
-st.header('Upload the respective dataset')
-problem_statement=st.text_input()
+st.title('AutoML')
+st.subheader('Project Statement')
+problem_statement=st.text_input('')
+st.subheader('Upload the respective dataset')
 uploaded_file=st.file_uploader('Upload the respective csv file',type=['csv','xlsx'])
+st.subheader('Project Path (else same project directory )')
+project_path=st.text_input('project Path')  
+
 if st.button('Submit'):
     pass
-st.header('Upload the respective dataset')
-uploaded_file=st.file_uploader('Upload the respective csv file',type=['csv','xlsx'])
-if st.button('Submit'):
-    pass
+
